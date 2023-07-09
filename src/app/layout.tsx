2d4e5a237369f134428ce27b5af2580ee6739451
mrprg <mohamadrezaasadi322@gmail.com>
2023-07-09 10:52:19 +0330

    add Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import MotionDiv from "@/components/MotionDiv";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
           <MotionDiv>
             <Navbar />
             {children}
+            <Footer />
           </MotionDiv>
         </body>
       </Provider>
