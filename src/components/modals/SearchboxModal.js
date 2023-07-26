b39405417ef4eb8704cfad54803403eefbdecef7
Fateme Kharazmi <73032767+faanid@users.noreply.github.com>
2023-07-26 22:36:12 +0430

    fix: Navigate to the searchbox

--- src/components/modals/SearchboxModal.js
+++ src/components/modals/SearchboxModal.js
@@ -1,9 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import Link from "next/link";
 
 const SearchBox = ({ open, onClose }) => {
   if (!open) {
     return null;
   }
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      window.location.href = "/searchbox";
+    }
+  };
   return (
     <div
       onClick={onClose}
@@ -55,27 +61,32 @@ const SearchBox = ({ open, onClose }) => {
 
               <div className="relative flex-grow mt-8">
                 <div className="absolute top-2 left-4 flex items-center pr-3 pointer-events-none">
-                  <svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z"
-                      fill="#353535"
-                    />
-                    <path
-                      d="M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z"
-                      fill="#353535"
-                    />
-                  </svg>
+                  <Link href="/searchbox">
+                    <svg
+                      className="cursor-pointer"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M11.5 21.75C5.85 21.75 1.25 17.15 1.25 11.5C1.25 5.85 5.85 1.25 11.5 1.25C17.15 1.25 21.75 5.85 21.75 11.5C21.75 17.15 17.15 21.75 11.5 21.75ZM11.5 2.75C6.67 2.75 2.75 6.68 2.75 11.5C2.75 16.32 6.67 20.25 11.5 20.25C16.33 20.25 20.25 16.32 20.25 11.5C20.25 6.68 16.33 2.75 11.5 2.75Z"
+                        fill="#353535"
+                      />
+                      <path
+                        d="M22.0004 22.7499C21.8104 22.7499 21.6204 22.6799 21.4704 22.5299L19.4704 20.5299C19.1804 20.2399 19.1804 19.7599 19.4704 19.4699C19.7604 19.1799 20.2404 19.1799 20.5304 19.4699L22.5304 21.4699C22.8204 21.7599 22.8204 22.2399 22.5304 22.5299C22.3804 22.6799 22.1904 22.7499 22.0004 22.7499Z"
+                        fill="#353535"
+                      />
+                    </svg>
+                  </Link>
                 </div>
+
                 <input
                   type="text"
                   placeholder="جستجو"
-                  className="text-right text-sm w-[409px] h-[40px] border rounded-lg focus:outline-none focus:ring focus:border-blue-300 pr-3 "
+                  className="text-right text-sm w-[409px] h-[40px] border rounded-lg focus:outline-none focus:ring focus:border-blue-300 pr-3"
+                  onKeyPress={handleKeyPress}
                 />
               </div>
             </div>
